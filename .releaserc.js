4c1a6a6fdd4a739d554fd05fbf91ec29c9527871
wangxingkang <wang_xingkang@qq.com>
2023-10-11 15:34:34 +0800

    chore: change release config

--- .releaserc.js
+++ .releaserc.js
@@ -1,11 +1,5 @@
 module.exports = {
-  branches: [
-    'master',
-    {
-      name: 'next',
-      prerelease: 'rc',
-    },
-  ],
+  branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
